c6a06d864246c50ed400d0216e6f827c4bf2f6c5
pyavchik <pyavchik@gmail.com>
2018-10-11 11:25:42 +0300

    fixed some bags

--- app.js
+++ app.js
@@ -12,21 +12,21 @@ searchUser.addEventListener('keyup', (e) => {
   const userText = e.target.value;
 
   if(userText !== ''){
-    //console.log(userText);
     // Make http call
     github.getUser(userText)
       .then(data => {
         if (data.profile.message === 'Not Found') {
           // Show alert
+          ui.showAlert('User not found', 'alert alert-danger');
 
         } else {
           // Show profile
           ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
         }
-        console.log(data);
       })
     } else {
       // Clear profile
+      ui.clearProfile();
   }    
-  
 });
--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class Github {
   constructor() {
     this.client_id = 'f4f70700604bca85f6fa';
     this.client_secret = '9b3beb220427a855ce101035f5f665248ed4cf69';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
     
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
